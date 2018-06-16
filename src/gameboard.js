73c7388bd0fe0c3bad33eaba79ec95fb5aa0086c
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-06-16 14:42:59 -0700

    Fix arrays from hell

--- src/gameboard.js
+++ src/gameboard.js
@@ -2,15 +2,28 @@ const Gameboard = size => {
   if (!size) {
     throw Error("Gameboard must have size")
   }
-  return {
+
+  const board = []
+  for (let i = 0; i < size; i++) {
+    board.push(new Array(size).fill(0))
+  }
+
+  const newGameboard = {
     size: size,
-    board: new Array(size).fill(new Array(size).fill(0)),
+    board: board,
+
+    setSquare: function (x, y, value) {
+      if (this.board[x] !== undefined && this.board[x][y] !== undefined) {
+        this.board[x][y] = value
+      } else {
+        console.error("no such square", [x, y])
+      }
+    },
 
     square: function (x, y) {
       // if (0) evaluates to false!
       if (this.board[x] !== undefined && this.board[x][y] !== undefined) {
-        const value = this.board[x][y]
-        return value
+        return this.board[x][y]
       } else {
         console.error("no such square", [x, y])
       }
@@ -32,6 +45,9 @@ const Gameboard = size => {
 
         if (this.square(pair.x, pair.y) === 0) {
           spaces.push(pair)
+        } else if (this.square(pair.x, pair.y) !== undefined) {
+          console.error("space not empty", pair)
+          return
         } else {
           // square not on board
           console.error("not on board", pair)
@@ -39,11 +55,13 @@ const Gameboard = size => {
         }
       }
 
-      spaces.forEach(pair => {
-        this.board[pair.x][pair.y] = ship
+      spaces.forEach((pair, index) => {
+        this.setSquare(pair.x, pair.y, { ship: ship, index: index })
       })
     }
   }
+
+  return newGameboard
 }
 
 export default Gameboard
--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -21,6 +21,13 @@ test("returns board square", () => {
   expect(gameboard.square(1, 1)).toEqual(42)
 })
 
+test("sets board square", () => {
+  const gameboard = Gameboard(3)
+  gameboard.setSquare(2, 2, 42)
+
+  expect(gameboard.square(2, 2)).toEqual(42)
+})
+
 test("doesn't return offboard square", () => {
   const gameboard = Gameboard(3)
   gameboard.board = [[0, 0, 0], [0, 42, 0], [0, 0, 0]]
@@ -33,9 +40,9 @@ test("places ship horizontally", () => {
   const gameboard = Gameboard(3)
 
   gameboard.place(ship, 0, 0, "h")
-  expect(gameboard.board[0][0]).toEqual(ship)
-  expect(gameboard.board[1][0]).toEqual(ship)
-  expect(gameboard.board[2][0]).toEqual(ship)
+  expect(gameboard.square(0, 0)).toMatchObject({ ship: ship, index: 0 })
+  expect(gameboard.square(1, 0)).toMatchObject({ ship: ship, index: 1 })
+  expect(gameboard.square(2, 0)).toMatchObject({ ship: ship, index: 2 })
 })
 
 test("places ship vertically", () => {
@@ -43,9 +50,9 @@ test("places ship vertically", () => {
   const gameboard = Gameboard(3)
 
   gameboard.place(ship, 0, 0, "v")
-  expect(gameboard.board[0][0]).toEqual(ship)
-  expect(gameboard.board[0][1]).toEqual(ship)
-  expect(gameboard.board[0][2]).toEqual(ship)
+  expect(gameboard.square(0, 0)).toMatchObject({ ship: ship, index: 0 })
+  expect(gameboard.square(0, 1)).toMatchObject({ ship: ship, index: 1 })
+  expect(gameboard.square(0, 2)).toMatchObject({ ship: ship, index: 2 })
 })
 
 test("won't place ship offboard", () => {
@@ -54,4 +61,12 @@ test("won't place ship offboard", () => {
 
   gameboard.place(ship, 1, 2, "h")
   expect(gameboard.board).toEqual(Gameboard(3).board)
+})
+
+test.skip("receives attack", () => {
+  const ship = Ship(3)
+  const gameboard = Gameboard(3)
+  gameboard.please(ship, 0, 0, "h")
+
+  gameboard.attack(0, 0)
 })
